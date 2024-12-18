5f84e881257a8d9ca201d5c7c7c6cd0afcf5eaa6
Hosain <cryptichasan925@gmail.com>
2024-12-18 21:57:11 +0600

    locomotive scroll

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,21 @@
+"use client"
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Journey from "@/components/Journey";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  // implement locomotive scroll
+  useEffect(() => {
+    const LoadLocomotiveScroll = async () => {
+      const LocoMotiveScroll = (await import("locomotive-scroll")).default;
+      new LocoMotiveScroll();
+    };
+    LoadLocomotiveScroll();
+  }, []);
   return (
     <div>
       <Hero />

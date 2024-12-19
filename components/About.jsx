398ffebfb29729ed67115b8b855bde2274f20541
Hosain <cryptichasan925@gmail.com>
2024-12-19 22:11:54 +0600

    text animation done

--- components/About.jsx
+++ components/About.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import AnimatedText from "./AnimatedText";
+ 
 
 const About = () => {
   return (
     <section className="bg-yellow-200 py-96" id="about">
-      About
+       <AnimatedText text="this is a title" textStyles="h2" />
     </section>
   );
 };

07fd43d81e0beef1433d9c1ee61a6f8c266fcd2e
Hosain <cryptichasan925@gmail.com>
2024-12-19 23:16:16 +0600

    about

--- components/About.jsx
+++ components/About.jsx
@@ -1,12 +1,57 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import AnimatedText from "./AnimatedText";
- 
 
 const About = () => {
   return (
-    <section className="bg-yellow-200 py-96" id="about">
-       <AnimatedText text="this is a title" textStyles="h2" />
+    <section className="relative pt-12 pb-24" id="about">
+      <div className="container mx-auto h-full">
+        <div className="h-full flex items-center justify-center">
+          <div className="hidden xl:flex flex-1 pl-8">
+            <div className="relative w-full max-w-[300px]">
+              {/* shape */}
+              <div className="w-[160px] h-[160px] bg-accent absolute -left-5 -top-5 -z-10"></div>
+              {/* image */}
+              <div className="rounded-tl-[8px] rounded-tr-[120px] w-full bg-[#e5f8f6] min-h-[350px] flex items-end justify-center">
+                <Image
+                  src="/hosain-about.png"
+                  width={350}
+                  height={478}
+                  quality={100}
+                  priority
+                  alt="Abu Hosain"
+                />
+              </div>
+              {/* rotating shape */}
+              <div className="absolute top-2/4 -right-[65px] flex items-center justify-center">
+                <motion.div
+                  animate={{
+                    rotate: [0, 360],
+                  }}
+                  transition={{
+                    duration: 10,
+                    ease: "linear",
+                    repeat: Infinity,
+                  }}
+                >
+                  <Image
+                    src="/assets/about/shape-1.svg"
+                    width={160}
+                    height={160}
+                    alt=""
+                  />
+                </motion.div>
+                <div className="absolute text-center text-white">
+                  <div>2+</div>
+                  <div>
+                    Years of <br /> Experience
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };

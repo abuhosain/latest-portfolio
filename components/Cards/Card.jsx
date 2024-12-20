f965418ec7390bbd1a1c0335f4c23e973f8e5aa3
Hosain <cryptichasan925@gmail.com>
2024-12-20 17:47:58 +0600

    animation done

--- components/Cards/Card.jsx
+++ components/Cards/Card.jsx
@@ -1,6 +1,24 @@
+const Card = ({
+  type,
+  logoUrl,
+  position,
+  duration,
+  description,
+  company,
+  istitution,
+  qualification,
+  name,
+  icon,
+}) => {
+  return (
+    <div className="w-full h-[300px] overflow-hidden flex items-center sticky top-12">
+      <div className="w-full h-[270px] border border-accent/80 bg-[#f4ffff] rounded-[8px]">
+      <div className="flex flex-col h-full">
 
-const Card = () => {
-  return <div>Card</div>;
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;

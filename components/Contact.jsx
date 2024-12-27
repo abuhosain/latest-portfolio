1477548234ee4f2c7f8dfd2f1b2c400835a00ce0
Hosain <cryptichasan925@gmail.com>
2024-12-27 20:17:01 +0600

    contact form setup done

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+
+// form server
+import { useForm, ValidationError } from "@formspree/react";
+
+import Image from "next/image";
+import { FaCheckCircle } from "react-icons/fa";
+
+import AnimatedText from "./AnimatedText";
 
 const Contact = () => {
-  return (
-    <section className='bg-cyan-200 py-96' id='contact'>Contact</section>
-  )
-}
+  const [state, handleSubmit] = useForm("xlddjvww");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
 
-export default Contact
+  const [showIcon, setShowIcon] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // clear the form after submission
+  useEffect(() => {
+    if (state.succeeded) {
+      setShowIcon(true);
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        message: "",
+      });
+
+      // hide the icon after 3 seconds
+      setTimeout(() => {
+        setShowIcon(false);
+      }, 3000);
+    }
+  }, [state.succeeded]);
+  return (
+    <section className="bg-cyan-200 py-96" id="contact">
+      Contact
+    </section>
+  );
+};
+
+export default Contact;

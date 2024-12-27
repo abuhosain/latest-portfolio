42d5a7b529d887d4d4b58c13f7969a2fdb93b279
Hosain <cryptichasan925@gmail.com>
2024-12-27 21:07:16 +0600

    contact form setup

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -38,14 +38,164 @@ const Contact = () => {
       });
 
       // hide the icon after 3 seconds
-      setTimeout(() => {
+      const timer = setTimeout(() => {
         setShowIcon(false);
       }, 3000);
+
+      // clean the timer after unmounting
+      return () => clearTimeout(timer);
     }
   }, [state.succeeded]);
+
+  // form data submit
+  const handleFormSubmit = () => {
+    e.preventDefault();
+    handleSubmit(formData);
+  };
+
   return (
-    <section className="bg-cyan-200 py-96" id="contact">
-      Contact
+    <section className="pt-8 xl:pt-12 pb-32" id="contact">
+      <div className="container mx-auto">
+        <div>
+          <div>
+            <AnimatedText
+              text="Let's Work Together"
+              textStyles="h2 mb-12 text-center xl:text-left"
+            />
+
+            {/* form */}
+            <form className="flex flex-col gap-6 w-full max-w-[480px]">
+              <div className="flex gap-8">
+                {/* firstname and lastname */}
+                <div className="flex-1">
+                  <label
+                    htmlFor="firstname"
+                    className="block text-sm mb-2 font-medium text-primary"
+                  >
+                    First Name <span className="text-accent">*</span>
+                    <input
+                      onChange={handleChange}
+                      type="text"
+                      name="firstname"
+                      value={formData.firstName}
+                      className="input"
+                      placeholder="First Name"
+                      required
+                      id="lastname"
+                    />
+                  </label>
+                </div>
+                <div className="flex-1">
+                  <label
+                    htmlFor="lastname"
+                    className="block text-sm mb-2 font-medium text-primary"
+                  >
+                    Last Name <span className="text-accent">*</span>
+                    <input
+                      onChange={handleChange}
+                      type="text"
+                      name="lastname"
+                      value={formData.lastName}
+                      className="input"
+                      placeholder="Last Name"
+                      required
+                      id="lastname"
+                    />
+                  </label>
+                </div>
+              </div>
+              {/* email field */}
+              <div>
+                <label
+                  htmlFor="firstname"
+                  className="block text-sm mb-2 font-medium text-primary"
+                >
+                  Email <span className="text-accent">*</span>
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    name="email"
+                    value={formData.email}
+                    className="input"
+                    placeholder="demo@gmail.com"
+                    required
+                    id="email"
+                  />
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                  />
+                </label>
+              </div>
+              {/* phone field */}
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block text-sm mb-2 font-medium text-primary"
+                >
+                  Phone <span className="text-accent">*</span>
+                  <input
+                    onChange={handleChange}
+                    type="number"
+                    name="phone"
+                    value={formData.phone}
+                    className="input"
+                    placeholder="your phone number"
+                    required
+                    id="phone"
+                  />
+                  <ValidationError
+                    prefix="Phone"
+                    field="phone"
+                    errors={state.errors}
+                  />
+                </label>
+              </div>
+              {/* message field */}
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm mb-2 font-medium text-primary"
+                >
+                  Message <span className="text-accent">*</span>
+                  <textarea
+                    onChange={handleChange}
+                    name="message"
+                    value={formData.message}
+                    className="textarea"
+                    rows={5}
+                    placeholder="Send me message"
+                    required
+                    id="message"
+                  />
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                  />
+                </label>
+              </div>
+
+              {/* submit button */}
+              <button
+                type="submit"
+                disabled={state.submitting}
+                className="btn btn-accent flex items-center justify-center gap-2"
+              >
+                {state.submitting ? (
+                  <span>Sending</span>
+                ) : (
+                  <>
+                    <FaCheckCircle />
+                    <span>Send Message</span>
+                  </>
+                )}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };

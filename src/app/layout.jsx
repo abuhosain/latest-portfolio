6020b023af276575b8dd8ad4fd3839987f866a41
Hosain <cryptichasan925@gmail.com>
2024-12-30 01:16:16 +0600

    login done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 import { Inter, Bebas_Neue } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import UserProvider from "../context/user.provider";
+import { NextUIProvider } from "@nextui-org/system";
+
+import { useRouter } from "next/navigation";
 
 const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -17,22 +20,28 @@ const bebasNeue = Bebas_Neue({
   variable: "--font-bebas",
 });
 
- 
-
 const queryClient = new QueryClient();
 
 export default function RootLayout({ children }) {
+  const router = useRouter();
+
   return (
     <html lang="en">
-        <head>
+      <head>
         <title>Abu Hosain</title>
-        <meta name="description" content="I am a full-stack developer, and I love to build web applications." />
+        <meta
+          name="description"
+          content="I am a full-stack developer, and I love to build web applications."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={`antialiased ${inter.variable} ${bebasNeue.variable}`}>
         <QueryClientProvider client={queryClient}>
           <UserProvider>
-            <Toaster /> {children}
+            <NextUIProvider navigate={router.push}>
+              <Toaster />
+              {children}
+            </NextUIProvider>
           </UserProvider>
         </QueryClientProvider>
       </body>
---
+++ src/components/form/PHInput.tsx
@@ -0,0 +1,38 @@
+"use client";
+ 
+import { useFormContext } from "react-hook-form";
+import { IInput } from "../../types";
+ 
+import { Input } from "@nextui-org/input";
+
+interface IProps extends IInput {}
+
+export default function PHInput({
+  variant = "bordered",
+  size = "md",
+  required = false,
+  type = "text",
+  label,
+  name,
+}: IProps) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const errorMessage = errors ? (errors[name]?.message as string) : ""; // Get the error message for the specific field
+  const isInvalid = !!errorMessage; // Check if there's an error
+  return (
+    <Input
+      {...register(name)}
+      errorMessage={errorMessage}
+      isInvalid={isInvalid}
+      
+      placeholder={label}
+      required={required}
+      size={size}
+      type={type}
+      variant={variant}
+      className="border rounded-md"
+    />
+  );
+}

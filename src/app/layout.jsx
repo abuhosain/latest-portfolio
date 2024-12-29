e6f5df54c156b2822d7f41f4e577d710b641c4ee
Hosain <cryptichasan925@gmail.com>
2024-12-30 00:03:29 +0600

    layout setup done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,30 +1,40 @@
- import {Inter, Bebas_Neue} from "next/font/google"
+"use client"
+import { Inter, Bebas_Neue } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import UserProvider from "../context/user.provider";
 
 const inter = Inter({
-  weight : ['100', "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets : ["latin"],
-  variable : "--font-inter"
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 const bebasNeue = Bebas_Neue({
-  weight : ["400"],
-  subsets : ["latin"],
-  variable : "--font-bebas"
-})
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-bebas",
+});
 
-export const metadata = {
-  title: "Abu Hosain",
-  description: "I'am a full-stack developer, I love to build web applications.",
-};
+ 
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased ${inter.variable} ${bebasNeue.variable}`}
-      >
-        {children}
+        <head>
+        <title>Abu Hosain</title>
+        <meta name="description" content="I am a full-stack developer, and I love to build web applications." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={`antialiased ${inter.variable} ${bebasNeue.variable}`}>
+        <QueryClientProvider client={queryClient}>
+          <UserProvider>
+            <Toaster /> {children}
+          </UserProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
--- src/services/Auth/index.ts
+++ src/services/Auth/index.ts
@@ -1,7 +1,10 @@
+"use server";
+
 import axiosInstance from "@/src/lib/AxiousInstance";
+import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 
-export const loginUser = async (userData) => {
+export const loginUser = async (userData: any) => {
   try {
     const { data } = await axiosInstance.post("/auth/login", userData);
     if (data.success) {
@@ -21,6 +24,22 @@ export const loginUser = async (userData) => {
 export const logOut = async () => {
   cookies().delete("accessToken");
   cookies().delete("refreshToken");
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = (await cookies()).get("accessToken")?.value;
+  let decodedToken = null;
+
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken);
+    return {
+      email: decodedToken?.email,
+      role: decodedToken?.role,
+      name: decodedToken?.name,
+    };
+  } else {
+    return decodedToken;
+  }
 };
 
 export const getNewAccessToken = async () => {

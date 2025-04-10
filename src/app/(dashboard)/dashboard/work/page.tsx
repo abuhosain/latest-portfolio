c4dd8835f921c9b1a5c3a752239de9bff9153a3d
Hosain <cryptichasan925@gmail.com>
2025-04-10 20:27:25 +0600

    work home page done

--- src/app/(dashboard)/dashboard/work/page.tsx
+++ src/app/(dashboard)/dashboard/work/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { useGetAllJourney } from "../../../../hooks/journey.hook";
+import SkillTable from "../../../../components/Dashboard/SkillTable";
+import { useGetAllWork } from "../../../../hooks/work.hook";
+import WorkTable from "../../../../components/Dashboard/WorkTable";
+
+const ManageWork = () => {
+  const { data, isPending } = useGetAllWork();
+  const [work, setWork] = useState<any[]>([]);
+  console.log(work);
+  useEffect(() => {
+    if (data?.data) {
+      
+      setWork(data?.data);
+    }
+  }, [data]);
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="lg:ml-4">
+      <h3 className="text-2xl font-bold mb-4 text-center">My Project</h3>
+      <Link
+        className="flex justify-end"
+        href="/dashboard/work/create-work"
+      >
+        <Button
+          className="mb-4 border rounded-full bg-accent text-primary px-4 py-2"
+          color="success"
+        >
+          Create works
+        </Button>
+      </Link>
+      <WorkTable works={work} isLoading={isPending} setWorks={setWork} />
+    </div>
+  );
+};
 
-export default page
+export default ManageWork;
